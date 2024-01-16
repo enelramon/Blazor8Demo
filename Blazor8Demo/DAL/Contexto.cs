a398a363868ce5e552f488f77aa776a9f3093b49
Enel Almonte <enel_almonte@ucne.edu.do>
2024-01-15 20:41:26 -0400

    Agregando el contexto

---
+++ Blazor8Demo/DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Blazor8Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor8Demo.DAL;
+
+public class Contexto: DbContext
+{
+    public DbSet<Prioridades> Prioridades { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options): base(options)
+    {
+        
+    }
+}
